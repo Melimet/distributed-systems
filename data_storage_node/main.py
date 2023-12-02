631b2c8d2f77bb97ebc713876991d6ec0455752d
Erkka Rahikainen <erkka.rahikainen@almamedia.fi>
2023-12-02 19:51:05 +0200

    Add sockets to storage node

---
+++ data_storage_node/main.py
@@ -0,0 +1,28 @@
+import socket  
+  
+def main():  
+    host = socket.gethostname()  
+    port = 5000  
+  
+    data_storage_node_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+    data_storage_node_socket.bind((host, port))  
+  
+    data_storage_node_socket.listen(1)  
+    print('Data storage node is listening on port 5000')  
+  
+    conn, address = data_storage_node_socket.accept()  
+    print("Connection from: " + str(address))  
+  
+    while True:  
+        data = conn.recv(1024).decode()  
+        if not data:  
+            break  
+        print("Received data from reverse proxy: " + str(data))  
+  
+        data = input(' -> ')  
+        conn.send(data.encode())  
+  
+    conn.close()  
+  
+if __name__ == '__main__':  
+    main()
