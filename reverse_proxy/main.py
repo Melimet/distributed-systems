7f35d89c781e395b4d12efead4fa2154f166dfc3
Erkka Rahikainen <erkka.rahikainen@almamedia.fi>
2023-12-02 19:51:16 +0200

    Create reverse proxy

---
+++ reverse_proxy/main.py
@@ -0,0 +1,29 @@
+import socket  
+  
+def main():  
+    host = socket.gethostname()  
+    port = 6000  
+  
+    reverse_proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+    reverse_proxy_socket.bind((host, port))  
+    reverse_proxy_socket.listen(1)  
+  
+    print('Reverse proxy is listening on port 6000')  
+  
+    while True:  
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+        client_socket.connect((host, 5000))  
+  
+        message = input(" -> ")  
+        while message.lower().strip() != 'bye':  
+            client_socket.send(message.encode())  
+            data = client_socket.recv(1024).decode()  
+  
+            print('Received from data storage node: ' + data)  
+  
+            message = input(" -> ")  
+  
+        client_socket.close()  
+  
+if __name__ == '__main__':  
+    main()  
