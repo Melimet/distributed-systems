6d4913d9f08bce95a6adb7b429b560d304c98f7f
Erkka Rahikainen <erkka.rahikainen@almamedia.fi>
2023-12-15 20:02:21 +0200

    generate docs

--- reverse_proxy/README.md
+++ reverse_proxy/README.md
@@ -10,3 +10,9 @@ python3 -m pip install -r requirements.txt
 ```
 
 Start it via `uvicorn main:app --reload`
+
+
+## How to use
+
+When the complete application is up via docker compose, you can send requests to reverse proxy with a tool such as postman. There is a postman.json in the root files which can be imported and used to test out the api.
+
--- reverse_proxy/client_server.py
+++ reverse_proxy/client_server.py
@@ -1,12 +1,30 @@
 import asyncio
 
 class ClientServer:
+    """
+    Represents a client-server communication handler.
+
+    Attributes:
+        MESSAGE_TYPE (str): The message type for communication.
+        SELECT (str): The select operation code.
+        UPSERT (str): The upsert operation code.
+        DELETE (str): The delete operation code.
+        host (str): The host address of the server.
+        nodes (list): The list of nodes to communicate with.
+    """
+
     MESSAGE_TYPE = "2"
     SELECT = "0"
     UPSERT = "1"
     DELETE = "2"
 
     def __init__(self, host):
+        """
+        Initializes a ClientServer instance.
+
+        Args:
+            host (str): The host address of the server.
+        """
         self.host = host
         self.nodes = [
             {"ip": "storage0", "port": "5120"},
@@ -16,6 +34,15 @@ class ClientServer:
         print("ClientServer initialized")
 
     async def handle_request(self, request):
+        """
+        Handles a client request.
+
+        Args:
+            request: The client request object.
+
+        Returns:
+            The response from the server.
+        """
         file_name = request.path_params["path"]
         node = self.nodes[2]
 
@@ -35,6 +62,16 @@ class ClientServer:
         return "Functionality not yet implemented"
 
     async def send_request_to_node(self, node, message):
+        """
+        Sends a request to a node.
+
+        Args:
+            node (dict): The node to send the request to.
+            message (str): The message to send.
+
+        Returns:
+            The response from the node.
+        """
         print(f"Sending request to {node['ip']}:{node['port']}")
         reader, writer = await asyncio.open_connection(node['ip'], node['port'])
 
@@ -49,4 +86,15 @@ class ClientServer:
         return response.decode()
 
     def format_message(self, operation, file_name, data):
+        """
+        Formats a message for communication.
+
+        Args:
+            operation (str): The operation code.
+            file_name (str): The file name.
+            data (str): The data.
+
+        Returns:
+            The formatted message.
+        """
         return f"{self.MESSAGE_TYPE}\n{operation}\n{file_name}\n{data}"
