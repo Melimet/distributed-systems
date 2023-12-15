088daa150168b163e712819dbbc59dcaa4679d7c
Erkka Rahikainen <erkka.rahikainen@almamedia.fi>
2023-12-15 20:38:56 +0200

    init node registry

---
+++ reverse_proxy/node_registry.py
@@ -0,0 +1,46 @@
+import uuid
+
+MESSAGE_TYPE = "5"
+
+class Node:
+    id: int
+    ip: str
+    port: str
+    successor_ip: str
+    successor_port: str
+
+    def __init__(self, ip: str, port: int):
+        self.id = uuid.uuid4()
+        self.ip = ip
+        self.port = port
+        self.successor_ip = ""
+        self.successor_port = ""
+
+nodes = []
+
+def getNodes():
+    return nodes
+
+def addNode(ip: str, port: int):
+    if not any(node.ip == ip and node.port == port for node in nodes):
+        nodes.append(Node(ip, port))
+        updateSuccessors(nodes)
+
+def removeNode(ip: str, port: int):
+    for node in nodes:
+        if node.ip == ip and node.port == port:
+            nodes.remove(node)
+            break
+
+def updateSuccessors(nodes):
+    if len(nodes) == 0:
+        return
+    
+    if len(nodes) == 1:
+        nodes[0].successor_ip = nodes[0].ip
+        nodes[0].successor_port = nodes[0].port
+        return
+
+    for i in range(len(nodes)):
+        nodes[i].successor_ip = nodes[(i + 1) % len(nodes)].ip
+        nodes[i].successor_port = nodes[(i + 1) % len(nodes)].port
