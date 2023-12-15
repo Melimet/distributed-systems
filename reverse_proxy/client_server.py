b610885c0d8e8af52bf60b610fe72e8763538aa8
Erkka Rahikainen <erkka.rahikainen@almamedia.fi>
2023-12-15 18:50:18 +0200

    Implement fetching of files

--- reverse_proxy/client_server.py
+++ reverse_proxy/client_server.py
@@ -1,5 +1,12 @@
 import asyncio
 
+MESSAGE_TYPE="2"
+
+SELECT = "0"
+UPSERT = "1"
+DELETE = "2"
+
+
 class ClientServer:
     def __init__(self, host):
         self.host = host
@@ -13,10 +20,13 @@ class ClientServer:
         nodes = ["localhost:5120", "localhost:5121", "localhost:5122"]
 
         if request.method == "GET":
+
+            MESSAGE_TO_SEND = MESSAGE_TYPE + "\n" + SELECT + "\n" + "kalevin_kotialbumi.zip"
+
             node_ip= "storage0"
             node_port= "5120"
-            response = await self.sendRequestToNode(node_ip, node_port, "SELECT")
-            return response + " perkele"
+            response = await self.sendRequestToNode(node_ip, node_port, MESSAGE_TO_SEND)
+            return response
 
         # Send request to data_storage_node
         ##result = await self.sendRequestToNode(nodes[0], request)
